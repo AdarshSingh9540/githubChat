5f7bedd3a76a05a6a815f7118c58bdf4b1a9f691
Adarsh Singh <singhadarsh9540@gmail.com>
2024-12-06 23:56:05 +0530

    fix: hide google auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -23,10 +23,12 @@ const handler = NextAuth({
             clientId: process.env.GITHUB_ID|| '',
             clientSecret: process.env.GITHUB_SECRET ||''
           }),
-          GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID||'',
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET||''
-          })
+
+          
+          // GoogleProvider({
+          //   clientId: process.env.GOOGLE_CLIENT_ID||'',
+          //   clientSecret: process.env.GOOGLE_CLIENT_SECRET||''
+          // })
           
       ],
       secret: process.env.NEXTAUTH_SECRET
